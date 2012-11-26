be6153fe46980c7b113d8e20af00145a90b1a42a
David Burke <david@david-Gazelle-Professional.(none)>
2012-11-25 21:15:58 -0500

    New version to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-report-builder",
-    version = "1.1",
+    version = "1.2",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("Query and Report builder for Django ORM"),
