6184df0d767fa523795baa1cf1fe7a949cb1d85f
David <david@localhost.(none)>
2013-04-30 20:01:33 -0400

    Added help_text for format string

--- report_builder/models.py
+++ report_builder/models.py
@@ -127,7 +127,7 @@ class Format(models.Model):
     """ A specifies a Python string format for e.g. `DisplayField`s. 
     """
     name = models.CharField(max_length=50, blank=True, default='')
-    string = models.CharField(max_length=300, blank=True, default='')
+    string = models.CharField(max_length=300, blank=True, default='', help_text='Python string format. Ex ${} would place a $ in front of the result.')
 
     def __unicode__(self):
         return self.name
