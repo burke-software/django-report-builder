3f925b0be36e01e3cd8a1ba1173f7f32b050d297
David Burke <dmbst32@gmail.com>
2014-10-03 21:57:39 -0400

    Fixed unit tests

--- test_settings.py
+++ test_settings.py
@@ -10,6 +10,15 @@ DATABASES = {
     }
 }
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 INSTALLED_APPS = (
     'report_builder',
     'django.contrib.auth',
@@ -19,7 +28,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'south',
 )
 
 ROOT_URLCONF = "test_urls"
