efb558fce4fdab0c4e305501e64f5a17ca59d0e6
David Burke <dmbst32@gmail.com>
2015-01-13 02:17:50 -0500

    Static file fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-report-builder",
-    version = "3.0.5",
+    version = "3.0.6",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("Query and Report builder for Django ORM"),
