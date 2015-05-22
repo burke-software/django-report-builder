274fd9eef4e0dde89998c0ce50fb41b64a7d86c7
David Burke <dmbst32@gmail.com>
2015-05-22 11:23:06 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-report-builder",
-    version = "3.1.7",
+    version = "3.1.8",
     author = "David Burke",
     author_email = "david@burkesoftware.com",
     description = ("Query and Report builder for Django ORM"),
