8309db92fe9729d107c8e91a3c12752eb1cbb583
abhishek <abhishek@machbtech.com>
2015-05-27 17:21:36 -0500

    description save from GUI

--- report_builder/api/serializers.py
+++ report_builder/api/serializers.py
@@ -58,7 +58,7 @@ class ReportNestedSerializer(ReportSerializer):
     class Meta:
         model = Report
         fields = (
-            'id', 'name', 'modified', 'root_model', 'root_model_name',
+            'id', 'name', 'description', 'modified', 'root_model', 'root_model_name',
             'displayfield_set', 'distinct', 'user_created', 'user_modified',
             'filterfield_set', 'report_file', 'report_file_creation')
         read_only_fields = ('report_file', 'report_file_creation')
