925c1d447d0c95a47e18e3cfe5435902282cb6e9
brawaga <brawaga@gmail.com>
2017-01-17 12:40:34 +0800

    Update utils.py
    Adaptation for Django 1.10.

--- report_builder/utils.py
+++ report_builder/utils.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from itertools import chain
 from numbers import Number
+import django
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.fields import FieldDoesNotExist
 from django.conf import settings
@@ -199,11 +200,19 @@ def get_model_from_path_string(root_model, path):
             except FieldDoesNotExist:
                 return root_model
             if direct:
-                if hasattr(field, 'related'):
-                    try:
-                        root_model = field.related.parent_model()
-                    except AttributeError:
-                        root_model = field.related.model
+                if django.VERSION >= (1, 10):
+                    rel = getattr(field, 'rel', None)
+                    if rel:
+                        try:
+                            root_model = rel.model
+                        except AttributeError:
+                            root_model = rel.related_model()
+                else:
+                    if hasattr(field, 'related'):
+                        try:
+                            root_model = field.related.parent_model()
+                        except AttributeError:
+                            root_model = field.related.model
             else:
                 if hasattr(field, 'related_model'):
                     root_model = field.related_model
