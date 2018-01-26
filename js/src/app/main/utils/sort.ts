50bd68da3c831e1888dd40748c60e76643072dd9
Natalie_Liles <Natalieliles@gmail.com>
2018-01-26 08:23:54 -0500

    Edit sort function.

--- js/src/app/main/utils/sort.ts
+++ js/src/app/main/utils/sort.ts
@@ -8,16 +8,31 @@ export const sortReports = (sortBy: string, reports: IReport[], ascending: boole
 
     const sortArray = reports.slice(0);
 
-        return sortArray.sort((a, b) => {
-            const x = `a.${sortBy}.toLowerCase()`;
-            const y = `b.${sortBy}.toLowerCase()`;
-            if (ascending) {
-                if (x < y) { return -1; }
-                if (x > y) { return 1; }
-            } else {
-                if (x < y) { return 1; }
-                if (x > y) { return -1; }
+            if (sortBy === 'user') {
+                return sortArray.sort((a, b) => {
+                    const x = a.user_created.first_name.toLowerCase();
+                    const y = b.user_created.first_name.toLowerCase();
+                    if (ascending) {
+                        if (x < y) { return -1; }
+                        if (x > y) { return 1; }
+                    } else {
+                        if (x < y) { return 1; }
+                        if (x > y) { return -1; }
+                    }
+                    return 0;
+                });
             }
-            return 0;
-        });
+
+            return sortArray.sort((a, b) => {
+                const x = a[sortBy].toLowerCase();
+                const y = b[sortBy].toLowerCase();
+                if (ascending) {
+                    if (x < y) { return -1; }
+                    if (x > y) { return 1; }
+                } else {
+                    if (x < y) { return 1; }
+                    if (x > y) { return -1; }
+                }
+                return 0;
+            });
 };
