35f8e84a507ea5138aedc190569757fd571f645c
Natalie_Liles <Natalieliles@gmail.com>
2018-01-30 13:52:23 -0500

    Edit filterSearch function.

--- js/src/app/main/utils/filterSearch.ts
+++ js/src/app/main/utils/filterSearch.ts
@@ -1,21 +1,32 @@
-// import { IField, IReport, IRelatedField } from '../../api.interfaces';
+import { IField, IReport, IRelatedField } from '../../api.interfaces';
 
- export const setSearch = (searchArray: any[], searchTerm: string) => {
+  export const setSearch = (searchArray: (IField|IReport|IRelatedField)[], searchTerm: string) => {
 
-    if (!searchTerm) {
-      return searchArray;
-    }
+      if (!searchTerm) {
+        return searchArray;
+      }
 
-    const term = searchTerm.toLowerCase();
-    return (searchArray.filter((report) => {
+      const isIRelatedField = (object: IField|IReport|IRelatedField): object is IRelatedField => {
+        return "verbose_name" in object;
+      };
+
+      const term = searchTerm.toLowerCase();
+      return (searchArray.filter((report) => {
 
       let name;
-      Object.keys(searchArray[0]).includes('verbose_name') ? 
-      name = report.verbose_name : name = report.name;
+      if (isIRelatedField(report)) {
+          name = report.verbose_name;
+      } else {
+          name = report.name;
+      }
 
       if (name) {
         name = name.toLowerCase();
       }
       return name.indexOf(term) >= 0;
-    }));
+  }));
 };
+
+
+
+  
