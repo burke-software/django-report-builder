24f4a79f621791667a04febf20eabc316d4b1c39
Joshua Marantz <jmarantz@thelabnyc.com>
2018-04-02 20:14:19 +0000

    comment on a hack 2.0

--- js/src/app/main/tabs/filter-tab/filter-input.component.ts
+++ js/src/app/main/tabs/filter-tab/filter-input.component.ts
@@ -49,8 +49,8 @@ export class FilterInputComponent implements OnChanges {
       this.date = date;
       this.time = time;
     }
-    // This hack made sure the filter_value is never null for boolean filter types
     if (changes.filterType && /^(?:isnull|max|min)$/.test(changes.filterType.currentValue) && this.value.length === 0 ) {
+      // settimeout is a hack because we probably shouldn't be emitting inside of ngOnChanges. Eventually this should be moved to some kind of serializer
       setTimeout(() => this.emitBoolean(false), 0)
     }
   }
