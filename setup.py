aa329993754c10ae3116032719939053156f0249
David Burke <dburke@thelabnyc.com>
2018-04-12 15:11:25 -0400

    Patch release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-report-builder",
-    version="5.0.0",
+    version="5.0.1",
     author="David Burke",
     author_email="david@burkesoftware.com",
     description=("Query and Report builder for Django ORM"),
