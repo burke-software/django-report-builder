61ee75eb0a2d534d6730925f4b426aa379b795e4
Harry White <harry.white424@gmail.com>
2019-01-24 16:47:25 +1100

    Restructure api test case, add permissions check test

--- report_builder/api/tests.py
+++ report_builder/api/tests.py
@@ -9,48 +9,62 @@ import json
 class ApiTestCase(TestCase):
 
     def setUp(self):
-        self.su = get_user_model().objects.create_superuser('su', 'su@example.com', 'su')
+        um = get_user_model()
+        self.superuser = um.objects.create_superuser('su', 'su@example.com', 'su')
+        self.regularuser = um.objects.create_user('user', 'user@example.com', 'user')
         self.client = APIClient()
 
-    def get_child_related_fields(self):
+    def get_json(self, url):
         self.client.login(username='su', password='su')
-        ct = ContentType.objects.get_by_natural_key('demo_models', 'child')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        return content
+
+    def post_json(self, url, data):
         response = self.client.post(
-            reverse('related_fields'),
-            {'field': '', 'model': ct.id, 'path': ''},
+            url,
+            data,
             format='json'
         )
         self.assertEqual(response.status_code, 200)
         content = json.loads(response.content)
         return content
 
+    def test_endpoint_not_accessable_by_regular_user(self):
+        self.client.login(username='user', password='user')
+        response = self.client.get('/report_builder/api/contenttypes/')
+        self.assertEqual(response.status_code, 403)
+
+    def get_related_fields_for_ct(self, app_label, model_name):
+        self.client.login(username='su', password='su')
+        ct = ContentType.objects.get_by_natural_key(app_label, model_name)
+        content = self.post_json(
+            reverse('related_fields'),
+            {'field': '', 'model': ct.id, 'path': ''},
+        )
+        return content
+
     def test_related_fields(self):
-        related_fields = self.get_child_related_fields()
+        related_fields = self.get_related_fields_for_ct('demo_models', 'child')
         self.assertEqual(len(related_fields), 1)
         self.assertEqual(related_fields[0]['field_name'], 'parent')
         self.assertTrue(related_fields[0]['included_model'])
 
     @override_settings(REPORT_BUILDER_EXCLUDE=['demo_models.person'])
     def test_related_fields_exclude(self):
-        related_fields = self.get_child_related_fields()
+        related_fields = self.get_related_fields_for_ct('demo_models', 'child')
         self.assertEqual(len(related_fields), 1)
         self.assertEqual(related_fields[0]['field_name'], 'parent')
         self.assertFalse(related_fields[0]['included_model'])
 
-    def get_content_types(self):
-        self.client.login(username='su', password='su')
-        response = self.client.get('/report_builder/api/contenttypes/')
-        self.assertEqual(response.status_code, 200)
-        content = json.loads(response.content)
-        return content
-
     def test_get_all_content_types(self):
         num_content_types = ContentType.objects.count()
-        response = self.get_content_types()
+        response = self.get_json('/report_builder/api/contenttypes/')
         self.assertEqual(len(response), num_content_types)
 
     @override_settings(REPORT_BUILDER_EXCLUDE=['demo_models.person'])
     def test_get_content_types_with_exclude(self):
         num_content_types = ContentType.objects.count()
-        response = self.get_content_types()
+        response = self.get_json('/report_builder/api/contenttypes/')
         self.assertEqual(len(response), num_content_types - 1)
