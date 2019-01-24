ef3100498562d9eb4138e1a8ceb02f352f94984c
Harry White <harry.white424@gmail.com>
2019-01-24 16:00:31 +1100

    Added test for hiding content types

--- report_builder/api/tests.py
+++ report_builder/api/tests.py
@@ -21,19 +21,36 @@ class ApiTestCase(TestCase):
             format='json'
         )
         self.assertEqual(response.status_code, 200)
-        return response
+        content = json.loads(response.content)
+        return content
 
     def test_related_fields(self):
-        response = self.get_child_related_fields()
-        related_fields = json.loads(response.content)
+        related_fields = self.get_child_related_fields()
         self.assertEqual(len(related_fields), 1)
         self.assertEqual(related_fields[0]['field_name'], 'parent')
         self.assertTrue(related_fields[0]['included_model'])
 
     @override_settings(REPORT_BUILDER_EXCLUDE=['demo_models.person'])
     def test_related_fields_exclude(self):
-        response = self.get_child_related_fields()
-        related_fields = json.loads(response.content)
+        related_fields = self.get_child_related_fields()
         self.assertEqual(len(related_fields), 1)
         self.assertEqual(related_fields[0]['field_name'], 'parent')
         self.assertFalse(related_fields[0]['included_model'])
+
+    def get_content_types(self):
+        self.client.login(username='su', password='su')
+        response = self.client.get('/report_builder/api/contenttypes/')
+        self.assertEqual(response.status_code, 200)
+        content = json.loads(response.content)
+        return content
+
+    def test_get_all_content_types(self):
+        num_content_types = ContentType.objects.count()
+        response = self.get_content_types()
+        self.assertEqual(len(response), num_content_types)
+
+    @override_settings(REPORT_BUILDER_EXCLUDE=['demo_models.person'])
+    def test_get_content_types_with_exclude(self):
+        num_content_types = ContentType.objects.count()
+        response = self.get_content_types()
+        self.assertEqual(len(response), num_content_types - 1)
